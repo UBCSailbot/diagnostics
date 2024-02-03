56c2cd97d1377c21155866ddeb7711cf27f4f288
Sam Dai <daisam1215@gmail.com>
2024-02-03 07:40:55 -0800

    add install directory to gitignore

--- src/main/diagnostics.h
+++ src/main/diagnostics.h
@@ -3,9 +3,9 @@
 
 /* Include Files */
 #include <iostream>
+#include <string>
 #include <unordered_map>
 #include <vector>
-#include <string>
 
 #include "boatTest_common.h"
 #include "commonUI.h"
@@ -15,11 +15,11 @@
 class App
 {
 public:
-    CommonUI *                          ui;
-    YamlParser *                        yaml_parser;
+    CommonUI *   ui;
+    YamlParser * yaml_parser;
 
     App();
-    int appGetUserSelection(int * selection);
+    int appGetUserSelection(std::string * selection);
     ~App();
 };
 
